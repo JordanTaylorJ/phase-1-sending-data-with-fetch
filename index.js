ec512486af230b1b677db3ca8a0789cab62e8520
JordanJoseph <jordant.joseph@outlook.com>
2022-03-07 17:58:37 -0700

    commit tree 0f5fafd82159e02a635f26e453d26eb5db27dc52

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+const configurationObject = {
+    method : 'POST',
+    headers : {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+    },
+    body : JSON.stringify({
+        name : "Steve",
+        email : "steve@steve.com",
+    })
+};
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response){
+        return response.json();
+    })
+    .then(document.append())
+    //.catch(DOmmm.append(message))
+}
+
